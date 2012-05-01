73e1eb30b798731aa4b45018e077815e5084763c
onidevl@gmail.com <onidevl@gmail.com@a2da9ee9-ab84-80b0-546b-f4dd4b6615b0>
2012-05-01 11:53:51 +0000

    result['query_parameters'].nil? 改为result['query_parameters'].empty? #metadata:<prefix>情况时，增加getResult['metadata'] = Hash.new

--- app/models/dataobject.rb
+++ app/models/dataobject.rb
@@ -3,12 +3,14 @@ class Dataobject
   key :objectURI, String
   key :objectID, String
   key :parentURI, String
-  key :reference, String
+  key :reference, String  #URI of a CDMI data object that will be pointed to by a reference. No other fields may be specified when creating a reference.
   key :domainURI, String
   key :capabilitiesURI, String
   key :completionStatus, String
   key :percentComplete, String
-  key :mimeType, String
-  key :metaData, Hash
+  
+  key :metadata, Hash
+  key :mimetype, String
   key :value, String
+  key :valuerange, String
 end
--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,17 @@
 class Domain
   include MongoMapper::Document
-  key :path, String, :required => true
-  key :data, Hash
-  key :referID, String
+  key :objectURI, String
+  key :objectID, String
+  key :parentURI, String
+  # key :reference, String
+  key :domainURI, String
+  key :capabilitiesURI, String
+  # key :completionStatus, String
+  # key :percentComplete, String
+
+  key :metadata, Hash
+  key :enabled, String
+  # key :snapshots, Array
+  key :children, Array
+  key :childrenrange, String
 end
