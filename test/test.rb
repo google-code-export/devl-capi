76e195c9e456a1721f3e60fbb3e2028496abb72c
onidevl@gmail.com <onidevl@gmail.com@a2da9ee9-ab84-80b0-546b-f4dd4b6615b0>
2012-05-02 08:52:35 +0000

    把全线的key由String型改为Symbol型，因此查询metadata的key prefix操作得用.to_s先转为String型
    body的JSON转化增加:symbolize_names选项，并事先判断json格式是否有效

--- test/test.rb
+++ test/test.rb
@@ -1,5 +1,27 @@
 require "json"
+require "pp"
 
-body_json      = JSON("")
+root = true
 
-p body_json
+f = {
+  :root => root
+}
+
+kg = {
+  "root" => root
+}
+kg[:root] = false
+
+kg.each { |k,v|
+  f.each_key { |mk|
+    p mk.to_s.start_with?(k)
+  }
+}
+
+def fun
+  return nil
+end
+
+metadata = ( mt = fun ; mt.nil? ? Array.new : mt) 
+
+p metadata
